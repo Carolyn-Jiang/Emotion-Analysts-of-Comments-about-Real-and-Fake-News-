f154ed0878db7d31a5348d120c6a278497eb0c2e
Yuechen Jiang <57229766+Carolyn-Jiang@users.noreply.github.com>
2021-11-03 20:23:52 -0400

    Add testing data process

--- data_pre-processing.py
+++ data_pre-processing.py
@@ -154,3 +154,130 @@ if __name__ == "__main__":
     most_common_tri = get_top_text_ngrams(df.text,10,3)
     most_common_tri = dict(most_common_tri)
     sns.barplot(x=list(most_common_tri.values()),y=list(most_common_tri.keys()))
+
+#################################################################################
+#                                                                               #
+#                     Data Visualization of test data                           #
+#                                                                               #
+#################################################################################
+
+if __name__ == "__main__":
+    # load the real news data and preview
+    df_test = pd.read_csv('fake_or_real_news.csv')
+    print('====================== Test Data Preview ======================')
+    print(df_test.head())
+    print('====================== Test Data Describe =====================')
+    df_test.describe()
+    print('Test Dataset Shape:','\n',df_test.shape)
+    print('Test Columns name:','\n',df_test.columns)
+    
+    # Data visualization
+    df_test['text'] = df_test['text'] + " " + df_test['title']
+    label = []
+    for i in df_test['label']:
+        if i == 'FAKE':
+            label.append(0)
+        elif i == 'REAL':
+            label.append(1)
+        else:
+            label.append(2)
+            
+    clean_text = []
+    for i in df_test['text']:
+        data_cleaning = clean.clean(text = i)
+        clean_text.append(data_cleaning.denoise_text(i))
+    del df_test['text']
+    
+    df = pd.DataFrame({'text':clean_text,'category':label})
+
+    print('=========== Comparie the Number of Real and Fake News ===========')
+    sns.set_style("darkgrid")
+    sns.countplot(df.category)
+    
+    print('============= Real News World Cloud Before Cleaning =============')
+    plt.figure(figsize = (20,20)) # Text that is not Fake
+    wc = WordCloud(max_words = 2000 , width = 1600 , height = 800 , stopwords = STOPWORDS).generate(" ".join(df[df.category == 1].text))
+    plt.imshow(wc , interpolation = 'bilinear')
+    print('============= Fake News World Cloud Before Cleaning =============')
+    plt.figure(figsize = (20,20)) # Text that is Fake
+    wc = WordCloud(max_words = 2000 , width = 1600 , height = 800 , stopwords = STOPWORDS).generate(" ".join(df[df.category == 0].text))
+    plt.imshow(wc , interpolation = 'bilinear')
+    
+    clean_text = []
+    for i in df['text']:
+        data_cleaning = clean.clean(text = i)
+        clean_text.append(data_cleaning.denoise_text(i))
+    del df['text']
+    df = pd.DataFrame({'text':clean_text,'category':df['category']})
+    # df.head()
+    # data_cleaning = clean()
+    # df['text'] = df['text'].apply(data_cleaning.denoise_text)
+    # WHEN I USE CLASS I'M UNABLE TO USE 'apply' FUNCTION, STILL NEED TO FIND OUT WHY, USING LOOPS ARE SLOW
+    print('============== Real News World Cloud After Cleaning =============')
+    plt.figure(figsize = (20,20)) # Text that is not Fake
+    wc = WordCloud(max_words = 2000 , width = 1600 , height = 800 , stopwords = STOPWORDS).generate(" ".join(df[df.category == 1].text))
+    plt.imshow(wc , interpolation = 'bilinear')
+    print('============== Fake News World Cloud After Cleaning =============')
+    plt.figure(figsize = (20,20)) # Text that is Fake
+    wc = WordCloud(max_words = 2000 , width = 1600 , height = 800 , stopwords = STOPWORDS).generate(" ".join(df[df.category == 0].text))
+    plt.imshow(wc , interpolation = 'bilinear')
+    # Number of characters in texts
+    fig,(ax1,ax2)=plt.subplots(1,2,figsize=(12,8))
+    text_len=df[df['category']==1]['text'].str.len()
+    ax1.hist(text_len,color='red')
+    ax1.set_title('Original text')
+    text_len=df[df['category']==0]['text'].str.len()
+    ax2.hist(text_len,color='green')
+    ax2.set_title('Fake text')
+    fig.suptitle('Characters in texts')
+    plt.show()
+    print('The distribution of both seems to be a bit different.','\n', 
+          '2500 characters in text is the most common in original text category', '\n', 
+          'while around 5000 characters in text are most common in fake text category.')
+    
+    # Number of words in each text
+    fig,(ax1,ax2)=plt.subplots(1,2,figsize=(12,8))
+    text_len=df[df['category']==1]['text'].str.split().map(lambda x: len(x))
+    ax1.hist(text_len,color='red')
+    ax1.set_title('Original text')
+    text_len=df[df['category']==0]['text'].str.split().map(lambda x: len(x))
+    ax2.hist(text_len,color='green')
+    ax2.set_title('Fake text')
+    fig.suptitle('Words in texts')
+    plt.show()
+    
+    # Average word length in a text
+    fig,(ax1,ax2)=plt.subplots(1,2,figsize=(20,10))
+    word=df[df['category']==1]['text'].str.split().apply(lambda x : [len(i) for i in x])
+    sns.distplot(word.map(lambda x: np.mean(x)),ax=ax1,color='red')
+    ax1.set_title('Original text')
+    word=df[df['category']==0]['text'].str.split().apply(lambda x : [len(i) for i in x])
+    sns.distplot(word.map(lambda x: np.mean(x)),ax=ax2,color='green')
+    ax2.set_title('Fake text')
+    fig.suptitle('Average word length in each text')
+    
+    corpus = get_corpus(df.text)
+    print('Top 5 Words','\n',corpus[:5])
+    
+    counter = Counter(corpus)
+    most_common = counter.most_common(10)
+    most_common = dict(most_common)
+    print('Numbers of most common words','\n',most_common)
+    
+    # Unigram Analysis
+    plt.figure(figsize = (16,9))
+    most_common_uni = get_top_text_ngrams(df.text,10,1)
+    most_common_uni = dict(most_common_uni)
+    sns.barplot(x=list(most_common_uni.values()),y=list(most_common_uni.keys()))
+    
+    # Bigram Analysis
+    plt.figure(figsize = (16,9))
+    most_common_bi = get_top_text_ngrams(df.text,10,2)
+    most_common_bi = dict(most_common_bi)
+    sns.barplot(x=list(most_common_bi.values()),y=list(most_common_bi.keys()))
+    
+    # Trigram Analysis
+    plt.figure(figsize = (16,9))
+    most_common_tri = get_top_text_ngrams(df.text,10,3)
+    most_common_tri = dict(most_common_tri)
+    sns.barplot(x=list(most_common_tri.values()),y=list(most_common_tri.keys()))
