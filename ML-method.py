4474f86f346b7dfaf5b0a3acfc437247ffe7c30d
Yuechen Jiang <57229766+Carolyn-Jiang@users.noreply.github.com>
2021-11-03 20:23:12 -0400

    Add testing with data out of the original dataset

--- ML-method.py
+++ ML-method.py
@@ -21,8 +21,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 
 # data pre-processing
-True_news = pd.read_csv('/Users/yuechenjiang/Desktop/project660/True.csv')
-Fake_news = pd.read_csv('/Users/yuechenjiang/Desktop/project660/Fake.csv')
+'''
+# This part only run on the first time
+True_news = pd.read_csv('/Users/yuechenjiang/Desktop/project660/code-data/True.csv')
+Fake_news = pd.read_csv('/Users/yuechenjiang/Desktop/project660/code-data/Fake.csv')
 True_news['category'] = 1
 Fake_news['category'] = 0
 df = pd.concat([True_news,Fake_news])
@@ -36,6 +38,33 @@ for i in df['text']:
     clean_text.append(data_cleaning.denoise_text(i))
 del df['text']
 df = pd.DataFrame({'text':clean_text,'category':df['category']})
+df.to_csv("combined.csv",index=False)
+'''
+
+def get_corpus(text):
+    words = []
+    for i in text:
+        for j in i.split():
+            words.append(j.strip())
+    return words
+
+def get_top_text_ngrams(corpus, n, g):
+    vec = CountVectorizer(ngram_range=(g, g)).fit(corpus)
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0) 
+    words_freq = [(word, sum_words[0, idx]) for word, idx in vec.vocabulary_.items()]
+    words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq[:n]
+
+df = pd.read_csv('combined.csv')
+X = df['text']
+Y = df['category']
+x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y , train_size=0.8, shuffle = True, test_size=0.2, random_state=1)
+tfidf_vectorizer = TfidfVectorizer(ngram_range=(1,2), max_features=15000, use_idf=True, stop_words='english')
+x_train = tfidf_vectorizer.fit_transform(x_train)
+x_test = tfidf_vectorizer.fit_transform(x_test)
+
+print('train:',x_train.shape,'\n','test:',x_test.shape)
 
 # model setup
 models = [LogisticRegression(solver='lbfgs'),        # Logistic regression
@@ -45,7 +74,8 @@ models = [LogisticRegression(solver='lbfgs'),        # Logistic regression
           AdaBoostClassifier(),                      # Adaptive gradient boost
           BaggingClassifier(),                       # Bagging algorithm
           GradientBoostingClassifier(),              # Gradient Boosting Algorithm
-          SVC(kernel = 'linear')]            
+          SVC(kernel = 'linear')]
+          # GaussianNB()]            
 
 model_name = ['LogisticRegression',
               'RandomForestClassifier',
@@ -54,12 +84,8 @@ model_name = ['LogisticRegression',
               'AdaBoostClassifier',
               'BaggingClassifier',
               'GradientBoostingClassifier',
-              'SVMClassifier']
-
-tfidf_vectorizer = TfidfVectorizer(use_idf=True, stop_words='english')
-X = tfidf_vectorizer.fit_transform(df['text'])
-Y = df['category']
-x_train, x_test, y_train, y_test = model_selection.train_test_split(X, Y , train_size=0.8,test_size=0.2, random_state=1)
+              'SVMClassifier',
+              'NaiveBayesClassifier']
 
 acc = []
 cms = []
@@ -68,13 +94,68 @@ for model in models:
     # model_acc = model.score(x_test, y_test)*100
     acc.append(model.score(x_test, y_test))
     y_pred = model.predict(x_test)
-    cm = confusion_matrix(y_test,y_pred)
+    cm = confusion_matrix(y_test,y_p
+                          red)
     cms.append(cm)
     print(model,'\n',cm)
+    
+from sklearn.naive_bayes import GaussianNB
+model = GaussianNB()
+model.fit(x_train.toarray(),y_train)
+acc.append(model.score(x_test.toarray(), y_test))
+y_pred = model.predict(x_test.toarray())
+cm = confusion_matrix(y_test,y_pred)
+cms.append(cm)
+print('GaussianNB()\n',cm)
 
-fig,ax=plt.subplots(2,4,figsize=(20,8))
+fig,ax=plt.subplots(3,3,figsize=(20,18))
 for i in range(len(cms)):
-    plt.subplot(2, 4, i+1)
+    plt.subplot(3, 3, i+1)
+    sns.heatmap(np.array(cms[i]),cmap= "Blues", linecolor = 'black' , xticklabels = ['Fake','Real'] , yticklabels = ['Fake','Real'])
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title(model_name[i])
+
+a = pd.DataFrame({"name": model_name, "acc": acc})
+a
+
+#####################################################################################
+#                                                                                   #
+#                                    Test Part                                      #
+#                                                                                   #
+#####################################################################################
+
+df.to_csv("text2.csv",index=False)
+
+x_test2 = df['text']
+y_test2 = df['label']
+
+tfidf_vectorizer = TfidfVectorizer(ngram_range=(1,2), max_features=15000, use_idf=True, stop_words='english')
+x_test2 = tfidf_vectorizer.fit_transform(x_test2)
+
+acc = []
+cms = []
+for model in models:
+    model.fit(x_train,y_train)
+    # model_acc = model.score(x_test, y_test)*100
+    acc.append(model.score(x_test2, y_test2))
+    y_pred = model.predict(x_test2)
+    cm = confusion_matrix(y_test2,y_pred)
+    cms.append(cm)
+    print(model,'\n',cm)
+    
+from sklearn.naive_bayes import GaussianNB
+model = GaussianNB()
+model.fit(x_train.toarray(),y_train)
+acc.append(model.score(x_test2.toarray(), y_test2))
+y_pred = model.predict(x_test2.toarray())
+cm = confusion_matrix(y_test2,y_pred)
+cms.append(cm)
+print('GaussianNB()\n',cm)
+
+fig,ax=plt.subplots(3,3,figsize=(20,18))
+for i in range(len(cms)):
+    plt.subplot(3, 3, i+1)
     sns.heatmap(np.array(cms[i]),cmap= "Blues", linecolor = 'black' , xticklabels = ['Fake','Real'] , yticklabels = ['Fake','Real'])
     plt.xlabel("Predicted")
     plt.ylabel("Actual")
