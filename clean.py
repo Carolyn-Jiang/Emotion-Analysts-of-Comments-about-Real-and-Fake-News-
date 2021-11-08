9488b461033fd849b174f25ac0318fc945742e3d
Yuechen Jiang <57229766+Carolyn-Jiang@users.noreply.github.com>
2021-11-07 23:34:20 -0500

    Add words lemmatization

--- clean.py
+++ clean.py
@@ -23,17 +23,41 @@ class clean:
     def remove_stopwords(self, text):
         final_text = []
         text = self.text
+        text = text.lower()
         stop = set(stopwords.words('english'))
-        punctuation = list(string.punctuation)
-        stop.update(punctuation)
+        # punctuation = list(string.punctuation)
+        # stop.update(punctuation)
+        stop.update(['make','toward','’s'])
         for i in text.split():
             if i.strip().lower() not in stop:
                 final_text.append(i.strip())        
         return " ".join(final_text)
     
+    def lemma_pinct(self, text):
+        text = self.text
+        punctuation = list(string.punctuation)
+        punctuation.extend(['“','”','’','...'])
+        text = word_tokenize(text)
+        nlp = spacy.load('en_core_web_sm')
+        texts = []
+        for token in text:
+            doc = nlp(" ".join(token))
+            texts.append([token.lemma_ for token in doc])
+        result = []
+        for i in text:
+            result.append("".join(i))
+        final = []
+        for word in result:
+            if word not in punctuation:
+                final.append(word)
+    
+        return ' '.join(final)
+        #return " ".join(texts)
+    
     # Removing the noisy text
     def denoise_text(self, text):
         text = self.strip_html(self.text)
         text = self.remove_between_square_brackets(text)
+        text = self.lemma_pinct(text)
         text = self.remove_stopwords(text)
         return text
